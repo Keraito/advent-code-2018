1c474b4687106510ed3762113f506cac93cd0597
Keraito <dragoon_v2_chak_shun@hotmail.com>
2018-12-03 12:17:34 +0100

    Remove ts as i didn't need it really

--- src/day3.js
+++ src/day3.js
@@ -1,7 +1,7 @@
 // '#123 @ 3,2: 5x4'
 // A claim like #123 @ 3,2: 5x4 means that claim ID 123 specifies a rectangle 3 inches from the left edge, 2 inches from the top edge, 5 inches wide, and 4 inches tall. Visually, it claims the square inches of fabric represented by # (and ignores the square inches of fabric represented by .) in the diagram below:
 
-export const mapClaim = (claim: string) => {
+export const mapClaim = claim => {
   const [id, token, margins, sizes] = claim.split(' ');
   const [leftMargin, topMargin] = margins
     .slice(0, margins.length - 1)
